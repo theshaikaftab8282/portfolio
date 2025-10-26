75cc474b2838b43a040deca22df4304b1766eaf4
tanseer123 <62325889+tanseer123@users.noreply.github.com>
2025-10-26 19:14:16 +0530

    404 error resolved

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/biz-analytics-folio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
